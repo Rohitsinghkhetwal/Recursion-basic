463246e4a2c36204509ef9d58db6dfce35791db6
Rohit singh <raj0eshsing0909@gmail.com>
2024-07-05 13:45:33 +0530

    feat: added the subsequence program using recursion

---
+++ Subsequence.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void SubSeq(int arr[], vector<int>dts, int sz, int index) {
+    // base case 
+    if(index == sz) {
+        for(auto it: dts) {
+            cout << it << " " ;
+        }
+        if(dts.size() == 0) {
+            cout << "{}" << endl;
+        }
+        cout << endl;
+        return;
+    }
+
+    dts.push_back(arr[index]);
+    SubSeq(arr, dts, sz, index + 1);
+    dts.pop_back();
+    SubSeq(arr, dts, sz, index + 1);
+
+}
+int main() {
+    cout << "Finding the subseqence of an array" << endl;
+    int arr[] = {3,1,2};
+    int n = 3;
+    vector<int>dts;
+    SubSeq(arr, dts, n, 0);
+    return 0;
+}
